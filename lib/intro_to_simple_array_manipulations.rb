9413e6ae1819a18b498a54c076cf6953a1079379
John Cho <johnycho16@gmail.com>
2019-03-01 22:49:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,25 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
+
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
   
+end
+
+def using_concat(array1,array2)
+  
+end
+
+def using_insert(array,element)
+  
+end
+
+def using_uniq(array)
+
+end
